df09d52c4109a685a8c6b974bb675ceb18e183f2
ronaldomb98 <ronaldomb98@gmail.com>
2018-01-14 11:01:49 -0500

    Added model for stoing profile feed items. created migration and added to django admin.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -36,7 +36,6 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Represents a "user profile" inside our system."""
 
@@ -64,3 +63,15 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Django uses this when it needs to conver the object to a string"""
 
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string."""
+
+        return self.status_text
